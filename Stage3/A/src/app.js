a7269fd4f1d75ca033566b0aab25f996f7645c73
Ayush Mittal <ayushheritage18@gmail.com>
2021-04-08 16:06:57 +0530

    Review comment suggestions fixed

--- Stage3/A/src/app.js
+++ Stage3/A/src/app.js
@@ -1,66 +1,88 @@
-function App( {dataService}){
+function App({ dataService }) {
+  let weekdays = dataService.WEEKDAYS;
 
-    let weekdays = dataService.WEEKDAYS;
-    
-    const [isModalVisible, setIsModalVisible] = React.useState( ()=> false );
-    
-    const displayModalHandler = function(setCondition){
-        setIsModalVisible(setCondition);
+  const [isModalVisible, setIsModalVisible] = React.useState(() => false);
+
+  const [statusVisiblity, setStatusVisiblity] = React.useState(-1);
+
+  const [updateRequest, setUpdateRequest] = React.useState(() => {
+    return { specificDayUpdate: "no", specificMealUpdate: "no", requestID: 0 };
+  });
+  // initialaizse  specificDayUpdate, specificDayUpdate with any random string to make unequal to any meal or day
+  const updateData = function (day, meal, foodItems) {
+    if (dataService.updateMeal(day, meal, foodItems)) {
+      // Hide Modal
+      setIsModalVisible(false);
+      // Update the data
+      setUpdateRequest((prevProp) => {
+        return {
+          requestID: 1 - prevProp.requestID,
+          specificDayUpdate: day,
+          specificMealUpdate: meal,
+        };
+      });
+      // Show sucess status
+      setStatusVisiblity(1);
+      // Hide status after 2 sec
+      setTimeout(setStatusVisiblity, 2000, -1);
+      // Return 1 to modal to clear modal fields
+      return 1;
+    } else {
+      // UNSUCESS - UPDATE
+      console.log("error occureed");
+
+      // Show sucess status
+      setStatusVisiblity(0);
+      // Hide status after 2 sec
+      setTimeout(setStatusVisiblity, 2000, -1);
+
+      // Not changing Modal state on unsuccessful updae to let user make edit in modal
+      // Return 0 to not clear fields
+      return 0;
     }
+  };
 
-    const [statusVisiblity , setStatusVisiblity ] = React.useState(-1);
+  const todayDay = weekdays[(new Date().getDay() - 1 + 7) % 7];
 
-    const [ updateRequest , setUpdateRequest ] = React.useState(()=> { return {specificDayUpdate:"no" , specificMealUpdate:"no",requestID:0 } } );
-                                                                                // initialaizse  specificDayUpdate, specificDayUpdate with any random string to make unequal to any meal or day
-    const updateData = function( day,meal,foodItems){
-        
-        if( dataService.updateMeal(day,meal,foodItems) ){
-            // Hide Modal
-            displayModalHandler(false);
-            // Update the data
-            setUpdateRequest( (prevProp ) => { return {requestID: 1- prevProp.requestID , specificDayUpdate:day , specificMealUpdate:meal } } );
-            // Show sucess status
-            setStatusVisiblity( 1);
-            // Hide status after 2 sec
-            setTimeout( setStatusVisiblity , 2000, -1 );
-            // Return 1 to modal to clear modal fields
-            return 1;
-        }
-        else{
-            // UNSUCESS - UPDATE
-            console.log( "error occureed");
+  return (
+    <React.Fragment>
+      <Heading type="1" id="main-heading" value="Diet Plan" />
 
-            // Show sucess status
-            setStatusVisiblity( 0);
-            // Hide status after 2 sec
-            setTimeout( setStatusVisiblity , 2000, -1 );
+      {isModalVisible ? (
+        <Modal
+          meals={dataService.MEALS}
+          weekdays={dataService.WEEKDAYS}
+          displayModalHandler={setIsModalVisible}
+          updateData={updateData}
+        />
+      ) : (
+        <React.Fragment></React.Fragment>
+      )}
 
-            // Not changing Modal state on unsuccessful updae to let user make edit in modal
-            // Return 0 to not clear fields
-            return 0;
-        }
-    }
+      <Today day={todayDay} dataService={dataService} {...updateRequest} />
 
+      {/* // Different component for schedule heading to compensate button in heading  */}
+      <WeekScheduleHeading displayModalHandler={setIsModalVisible} />
 
-    const todayDay = weekdays[ (new Date().getDay() -1 + 7 )%7 ] ;
+      {weekdays
+        .slice(weekdays.indexOf(todayDay), 7)
+        .concat(weekdays.slice(0, weekdays.indexOf(todayDay)))
+        .map((w) => {
+          return (
+            <Weekday
+              day={w}
+              key={w}
+              dataService={dataService}
+              {...updateRequest}
+            />
+          );
+        })}
 
-    return(
-     <React.Fragment>
-     
-        <Heading type="1" id="main-heading" value="Diet Plan"  />
-        
-        <Modal meals = {dataService.MEALS} weekdays ={dataService.WEEKDAYS} visiblity={isModalVisible} displayModalHandler ={displayModalHandler} updateData={updateData} />
-        
-        <Today day ={todayDay} dataService={dataService} {...updateRequest}/>
-        
-        {/* // Different component for schedule heading to compensate button in heading  */}
-        <WeekScheduleHeading displayModalHandler= {displayModalHandler}   />
-
-        { weekdays.slice(weekdays.indexOf(todayDay),7).concat(weekdays.slice( 0 ,weekdays.indexOf(todayDay) ) )
-        .map( w  =>{ return <Weekday day={w} key={ w} dataService={dataService} {...updateRequest}  /> } )}
-
-        <Status isSuccess = {statusVisiblity}/>
-     </React.Fragment>
-    )
-
+      {statusVisiblity !== -1 ? (
+        <Status isSuccess={statusVisiblity} />
+      ) : (
+        <React.Fragment></React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
--- Stage3/A/src/components/modal.js
+++ Stage3/A/src/components/modal.js
@@ -1,108 +1,208 @@
-const Modal = React.memo( function Modal( {visiblity , displayModalHandler ,updateData , meals, weekdays} ){
-
-
+// init to initialse reducer state
+function init() {
+  return {
     // Day Selected by user
-    const [day, setDay ] = React.useState("monday");
-    // Meal selected by user 
-    const [meal, setMeal] = React.useState("breakfast");
+    day: "monday",
+    // Meal selected by user
+    meal: "breakfast",
     // Input Entered by user
-    const [ foodItems , setFoodItems] = React.useState( "" );
+    foodItems: "",
     // Parsed Item after checking repeatition and trim
-    const [ parsedFoodItems , setParsedFoodItems] = React.useState( [] );
-   
-   
-    // Using Reference to store deom reference of parsed item view and scroll to last for userview
-    const dom_ref_ul = React.useRef();
-    
-    // On item change update parsed item list 
-    function onChangeHandler(event ){
+    parsedFoodItems: [],
+  };
+}
 
-        let items = event.target.value.split(";").filter(x => x.trim().length>=1 ).map( function(x){ return x.trim().split(" ").filter(x => x.trim().length>=1 ).map(function(a){ return a[0].toUpperCase() + a.substring(1) ; } ).join(" ") ; } );           
-        // Avoid Duplicates
-        let taken = new Map();
-        for( const ele of items){
-            if(  ele !="" && ! taken.has(ele.toLowerCase()  ) ){
-                taken.set( ele.toLowerCase() , ele );
+// Reducer to skip use of multiple useStates
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_DAY":
+      return { ...state, day: action.payload };
+    case "SET_MEAL":
+      return { ...state, meal: action.payload };
+    case "SET_FOOD_ITEMS":
+      return { ...state, foodItems: action.payload };
+    case "SET_PARSED_FOOD_ITEMS":
+      return { ...state, parsedFoodItems: action.payload };
+    case "RESET":
+      return init();
+    default:
+      throw new Error("Invalid State Change in Modal");
+  }
+}
+
+const Modal = function Modal({
+  displayModalHandler,
+  updateData,
+  meals,
+  weekdays,
+}) {
+  // State for handling modal field
+  const [state, dispatch] = React.useReducer(reducer, null, init);
+  // Using Reference to store deom reference of parsed item view and scroll to last for userview
+  const dom_ref_ul = React.useRef();
+
+  // On item change update parsed item list
+  function onChangeHandler(event) {
+    let items = event.target.value
+      .split(";")
+      .filter((x) => x.trim().length >= 1)
+      .map(function (x) {
+        return x
+          .trim()
+          .split(" ")
+          .filter((x) => x.trim().length >= 1)
+          .map(function (a) {
+            return a[0].toUpperCase() + a.substring(1);
+          })
+          .join(" ");
+      });
+    // Avoid Duplicates
+    let taken = new Map();
+    for (const ele of items) {
+      if (ele != "" && !taken.has(ele.toLowerCase())) {
+        taken.set(ele.toLowerCase(), ele);
+      }
+    }
+    items = Array.from(taken.values());
+    //Update Parsed items
+    dispatch({ type: "SET_PARSED_FOOD_ITEMS", payload: items });
+    // Input to be same as user entered
+    dispatch({ type: "SET_FOOD_ITEMS", payload: event.target.value });
+
+    //  Scroll to bottom
+    if (dom_ref_ul.current) {
+      dom_ref_ul.current.scrollTop = dom_ref_ul.current.scrollHeight;
+    }
+  }
+
+  handleModalClose = () => {
+    displayModalHandler(false);
+  };
+
+  submitHandle = () => {
+    // Check if update is succesful
+    if (
+      updateData(
+        state.day,
+        state.meal,
+        state.foodItems
+          .split(";")
+          .filter((x) => x.trim().length >= 1)
+          .map(function (x) {
+            return x
+              .trim()
+              .split(" ")
+              .filter((x) => x.trim().length >= 1)
+              .map(function (a) {
+                return a[0].toUpperCase() + a.substring(1);
+              })
+              .join(" ");
+          })
+      )
+    ) {
+      // Reset the form fields
+      resetForm();
+    }
+    // else dont reset the form
+  };
+
+  function resetForm() {
+    // Reset the form fields
+    dispatch({ type: "RESET" });
+  }
+
+  return (
+    <div className="modal-cover">
+      <div className="modal">
+        <button className="btn--right btn" onClick={handleModalClose}>
+          X
+        </button>
+        <button className="btn--simple" onClick={resetForm}>
+          clear
+        </button>
+
+        <Heading
+          type="3"
+          value="Edit Schedule"
+          className="main-heading"
+        ></Heading>
+
+        {/* Form for user input */}
+        <form onSubmit={(event) => event.preventDefault()}>
+          <label htmlFor="days-select">Day: </label>
+          <select
+            name="days"
+            id="days-select"
+            value={state.day}
+            onChange={(e) =>
+              dispatch({ type: "SET_DAY", payload: e.target.value })
             }
-        }
-        items = Array.from( taken.values() );
-        //Update Parsed items
-        setParsedFoodItems( items); 
-        // Input to be same as user entered
-        setFoodItems(event.target.value) ;
+            onBlur={(e) =>
+              dispatch({ type: "SET_DAY", payload: e.target.value })
+            }
+          >
+            {weekdays.map((d) => (
+              <option value={d} key={d}>
+                {d[0].toUpperCase() + d.substring(1)}
+              </option>
+            ))}
+          </select>
+          <br></br>
 
-        //  Scroll to bottom
-        if( dom_ref_ul.current ){
-            dom_ref_ul.current.scrollTop = dom_ref_ul.current.scrollHeight;
-        }
-    }
+          <label htmlFor="meal-select">Time Of Day: </label>
+          <select
+            name="meal"
+            id="meal-select"
+            value={state.meal}
+            onChange={(e) =>
+              dispatch({ type: "SET_MEAL", payload: e.target.value })
+            }
+            onBlur={(e) =>
+              dispatch({ type: "SET_MEAL", payload: e.target.value })
+            }
+          >
+            {meals.map((d) => (
+              <option value={d} key={d}>
+                {d[0].toUpperCase() + d.substring(1)}
+              </option>
+            ))}
+          </select>
+          <br></br>
 
+          <label htmlFor="food-input">Food Items (separated by ;) : </label>
 
-    handleModalClose= () =>{
-        displayModalHandler(false);
-    }
+          <input
+            name="food"
+            id="food-input"
+            type="text"
+            value={state.foodItems}
+            onChange={onChangeHandler}
+            onBlur={onChangeHandler}
+          ></input>
+          <br />
 
-    submitHandle = ()=>{
-        // Check if update is succesful
-        if( updateData( day,meal, 
-            foodItems.split(";").filter(x => x.trim().length>=1 ).map( function(x){ return x.trim().split(" ").filter(x => x.trim().length>=1 ).map(function(a){ return a[0].toUpperCase() + a.substring(1) ; } ).join(" ") ; } ) )){            
-                // Reset the form fields
-                resetForm();
-        }
-        // else dont reset the form 
-    }
+          {/* Parsed enteried items in modal view */}
+          {state.parsedFoodItems.length > 0 ? (
+            <label htmlFor="modal-ul">Preview : </label>
+          ) : (
+            <React.Fragment></React.Fragment>
+          )}
 
-    function resetForm(){
-        // Reset the form fields
-        setDay("monday");
-        setMeal("breakfast");
-        setFoodItems("");
-        setParsedFoodItems([]);
-    }
+          {state.parsedFoodItems.length > 0 ? (
+            <ul ref={dom_ref_ul} id="modal-ul" className="modal__ul">
+              {state.parsedFoodItems.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          ) : (
+            <React.Fragment></React.Fragment>
+          )}
+        </form>
 
-
-    return (
-        <div  className="modal-cover" style={ {display:(visiblity?"block":"none") }} >
-            <div className="modal">
-                <button className="btn--right btn" onClick ={handleModalClose} >X</button>
-                <button className="btn--simple" onClick={resetForm}>clear</button>
-                
-                <Heading type="3" value="Edit Schedule" className="main-heading" ></Heading>   
-                
-                {/* Form for user input */}
-                <form onSubmit={ (event) =>     event.preventDefault() }>
-                
-                    <label htmlFor="days-select">Day: </label>
-                      <select name = "days" id = "days-select" value={day} onChange={(e) => setDay(e.target.value)} onBlur={(e) => setDay(e.target.value)}>
-                        {weekdays.map( (d) => <option value={d} key={d} >{d[0].toUpperCase() + d.substring(1) }</option> )}
-                        </select>
-                        <br></br>
-
-                    <label htmlFor="meal-select">Time Of Day: </label>
-                        <select name = "meal" id = "meal-select" value={meal} value={meal} onChange={(e) => setMeal( e.target.value)} onBlur={(e) => setMeal(e.target.value)}>
-                        {meals.map( (d) => <option value={d} key={d}>{d[0].toUpperCase() + d.substring(1) }</option> )}  
-                        </select>
-                    <br></br>
-
-                    <label htmlFor="food-input">Food Items (separated by ;) : </label>
-                    
-                    <input name="food" id="food-input" type="text" value={foodItems} onChange={onChangeHandler} onBlur={onChangeHandler}></input>
-                    <br/>
-                    
-                    {/* Parsed enteried items in modal view */}
-                    { parsedFoodItems.length>0?
-                        <label htmlFor="modal-ul">Preview : </label>:<React.Fragment></React.Fragment> }
-
-                    { parsedFoodItems.length>0?
-                        <ul ref={dom_ref_ul} id ="modal-ul" className="modal__ul">
-                        {   parsedFoodItems.map((item) => <li key={item} >{item}</li> )}
-                        </ul>:<React.Fragment></React.Fragment>    }             
-                </form>
- 
-                <button className="modal__submit-btn" onClick={submitHandle}>Update Changes</button>
-            </div>
-        </div>
-
-    );
-} , areEqual =function(prevProp,nextProp ){
-    return nextProp.visiblity == prevProp.visiblity;} ) ;  // Rerender Modal due to parent state only if visiblity is updated
+        <button className="modal__submit-btn" onClick={submitHandle}>
+          Update Changes
+        </button>
+      </div>
+    </div>
+  );
+};
