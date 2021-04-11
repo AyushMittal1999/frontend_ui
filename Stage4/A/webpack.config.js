909024cba78030c99975dd3e7cc435621f11c819
unknown <ayushheritage18@gmail.com>
2021-04-11 21:33:16 +0530

    Added polyfill to support old browser

--- Stage4/A/webpack.config.js
+++ Stage4/A/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   entry: "./src/index.js",
   mode: mode,
   output: {
+    publicPath: "",
     filename: "bundle.js",
   },
 
@@ -52,6 +53,7 @@ module.exports = {
     port: port,
     historyApiFallback: true,
     open: true,
+
     contentBase: "public/",
   },
 };
